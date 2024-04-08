cc1de8678aa440ccab624cf256f272602f7043b4
フランカツ!! <kole3600@gmail.com>
2024-04-07 17:45:51 -0700

    test commit
    test commit

--- task1.py
+++ task1.py
@@ -15,6 +15,7 @@ What is your name:Dwayne Johnson
 What ir your email:rock@aol.com
 
 Your name is Dwayne Johnson, and your email is rock@aol.com
+(aol? does dwayne johnson also use netscape navigator?)
 
 What is your name:Jackie Chan
 What ir your email:crazyAsian@qq.com
@@ -22,3 +23,6 @@ What ir your email:crazyAsian@qq.com
 Your name is Jackie Chan, and your email is crazyAsian@qq.com
 
 """
+N = input("Hello! What is your name? ")
+EM = input("I will also need your email. ")
+print(f"Your name is {N}, and your email is {EM}, did i get that correct?")
